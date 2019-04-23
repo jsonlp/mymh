2c41819ddfe257fe1e1732e99a2749c96396e14f
liaopan <353871489@qq.com>
2019-04-23 16:19:55 +0800

    体活完善

--- app/src/main/java/com/lpan/my/mh/fragment/CalculateTiliFragment.java
+++ app/src/main/java/com/lpan/my/mh/fragment/CalculateTiliFragment.java
@@ -10,18 +10,22 @@ import com.lpan.my.mh.R;
 
 /**
  * Created by lpan on 2019/4/11.
- * 体活总值=50+等级*50+生活技能*4
+ * 体活总值=50+等级*5+生活技能*4
  * 体活回复=INT(上限*0.01)+INT(等级*0.02)+2
  */
 public class CalculateTiliFragment extends BaseFragment {
-
     private EditText mLvEt;
-    private EditText mTiliEt;
-    private EditText mHuoliliEt;
+    private EditText mJianTiEt;
+    private EditText mYangShengEt;
     private EditText mTiliCurEt;
     private EditText mHuoliliCurEt;
     private Button mButton;
     private TextView mResultTv;
+
+    /**
+     * 方寸每小时216点活力 108张FF 54000钱 5400魔
+     * @return
+     */
 
 
     @Override
@@ -33,8 +37,8 @@ public class CalculateTiliFragment extends BaseFragment {
     protected void initView(View rootView) {
         super.initView(rootView);
         mLvEt = rootView.findViewById(R.id.lv_et);
-        mTiliEt = rootView.findViewById(R.id.tili_et);
-        mHuoliliEt = rootView.findViewById(R.id.huoli_et);
+        mJianTiEt = rootView.findViewById(R.id.jian_ti_et);
+        mYangShengEt = rootView.findViewById(R.id.yang_sheng_et);
         mTiliCurEt = rootView.findViewById(R.id.tili_cur_et);
         mHuoliliCurEt = rootView.findViewById(R.id.huoli_cur_et);
         mButton = rootView.findViewById(R.id.calculate_bt);
@@ -50,8 +54,11 @@ public class CalculateTiliFragment extends BaseFragment {
 
     private void calculate() {
         int lv = TextUtils.isEmpty(mLvEt.getText().toString()) ? 0 : Integer.parseInt(mLvEt.getText().toString());
-        int tili_total = TextUtils.isEmpty(mTiliEt.getText().toString()) ? 0 :Integer.parseInt(mTiliEt.getText().toString());
-        int huoli_total = TextUtils.isEmpty(mHuoliliEt.getText().toString()) ? 0 :Integer.parseInt(mHuoliliEt.getText().toString());
+        int jian_ti_lv = TextUtils.isEmpty(mJianTiEt.getText().toString()) ? 0 :Integer.parseInt(mJianTiEt.getText().toString());
+        int yang_sheng_lv = TextUtils.isEmpty(mYangShengEt.getText().toString()) ? 0 :Integer.parseInt(mYangShengEt.getText().toString());
+        int tili_total = 50+lv*5+jian_ti_lv*4;
+        int huoli_total=50+lv*5+yang_sheng_lv*4;
+
         int tili_total_cur = TextUtils.isEmpty(mTiliCurEt.getText().toString()) ? 0 :Integer.parseInt(mTiliCurEt.getText().toString());
         int huoli_total_cur = TextUtils.isEmpty(mHuoliliCurEt.getText().toString()) ? 0 :Integer.parseInt(mHuoliliCurEt.getText().toString());
 
@@ -69,9 +76,15 @@ public class CalculateTiliFragment extends BaseFragment {
         int huoli_time_extra = (huoli_total + huoli_total_extra - huoli_total_cur) / huoli_per_5m * 5;
 
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("体力每5分钟回复");
+        stringBuilder.append("体力=");
+        stringBuilder.append(tili_total);
+        stringBuilder.append("(");
+        stringBuilder.append(tili_total_extra);
+        stringBuilder.append(")");
+
+        stringBuilder.append(",每5分钟回复");
         stringBuilder.append(tili_per_5m);
-        stringBuilder.append(",体力从");
+        stringBuilder.append(",从");
         stringBuilder.append(tili_total_cur);
         stringBuilder.append("增加到");
         stringBuilder.append(tili_total);
@@ -81,9 +94,14 @@ public class CalculateTiliFragment extends BaseFragment {
         stringBuilder.append(tili_time_extra);
         stringBuilder.append("分钟\n");
 
-        stringBuilder.append("活力每5分钟回复");
+        stringBuilder.append("活力=");
+        stringBuilder.append(huoli_total);
+        stringBuilder.append("(");
+        stringBuilder.append(huoli_total_extra);
+        stringBuilder.append(")");
+        stringBuilder.append(",每5分钟回复");
         stringBuilder.append(huoli_per_5m);
-        stringBuilder.append(",活力从");
+        stringBuilder.append(",从");
         stringBuilder.append(huoli_total_cur);
         stringBuilder.append("增加到");
         stringBuilder.append(huoli_total);
